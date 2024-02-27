d8809436e12fba27b504756ac0d9693714df6478
Joao Patricio <ijpatricio@gmail.com>
2024-02-27 19:19:42 +0000

    Install Filament and create a Filament Theme.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/filament/admin/theme.css',
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/**/*.php',
+                'resources/views/**/*.php',
+            ],
         }),
     ],
 });
